539f1f9617d1f8df52f8f1383417c482c374a0b3
SiRaCCC <ncom555@gmail.com>
2026-06-27 17:54:49 +0700

    auth update 1

--- frontend/src/page/auth.tsx
+++ frontend/src/page/auth.tsx
@@ -1,7 +1,22 @@
-
+import styled from "styled-components";
+import LoginComponent from "#component/auth.tsx";
 export default function Auth ()
 {
-  return <>
-  <h1>Hello There!</h1>
-  </>;
+  return <View>
+      <LoginComponent/>
+  </View>;
 }
+
+const View = styled.div `
+  
+      position: absolute;
+      inset: 0px;
+      width: 100%;
+      height: 100%;
+  
+      display: flex; 
+      flex-direction: column;
+      flex-wrap: nowrap;
+      align-items: center;
+      justify-content: center;
+`
